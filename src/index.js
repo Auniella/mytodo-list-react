8e92cd32566cda9e54745d56f4757e94b2ecccc9
Auniella <152399852+Auniella@users.noreply.github.com>
2024-06-04 17:39:38 +0000

    Update index.js réglages dates

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 
 const DATA = [
-  { name: "Manger", completed: true, id: "note1" },
-  { name: "Dormir", completed: false, id: "note2" },
-  { name: "Marcher", completed: false, id: "note3" },
+  { name: "Manger", completed: true, id: "note1", date: "01/01/2024 01:01" },
+  { name: "Dormir", completed: false, id: "note2", date: "02/01/2024 01:02" },
+  { name: "Marcher", completed: false, id: "note3", date: "03/01/2024 01:03" },
 ];
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
